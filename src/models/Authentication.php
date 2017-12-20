47f7f2d582f507c43dd8d66d58d5a13a8d67d6c2
Ober <lucasober@hotmail.fr>
2017-12-20 15:10:40 +0100

    Ajout du squelette de Authentication

---
+++ src/models/Authentication.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+$_SESSION[authSessionVar];
+
+class Authentication{	
+	
+	function authenticate(){
+		
+	}
+	
+	function loadProfile($uid){
+		
+	}
+	
+	function checkAccessRights($required){
+		
+	}
+	
+	function createUser($uid,$userRights){
+		
+	}
+}
+
+
